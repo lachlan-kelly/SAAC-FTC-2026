3c8f56b347156caaee6b538399aad935b856e02c
lachlan-kelly <lachlan.kelly@phasezero.com.au>
2026-09-09 16:03:33 +1000

    new colour works

--- Autonomous/quadMotor.Mecnum.Auto_v1.5.1/main.java
+++ Autonomous/quadMotor.Mecnum.Auto_v1.5.1/main.java
@@ -1,9 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
-<<<<<<< HEAD
-=======
 import android.graphics.Color;
->>>>>>> ea805e9 (new auto code in testing)
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -11,8 +8,8 @@ import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
 import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 import com.qualcomm.robotcore.hardware.SwitchableLight;
 
-@Autonomous(name = "DriveUntillRed_Test", group = "Auto")
-public class DriveUntillRed_Test extends LinearOpMode {
+@Autonomous(name = "SensorColor", group = "Auto")
+public class SensorColor extends LinearOpMode {
 
     private DcMotor frontLeft;
     private DcMotor frontRight;
@@ -23,15 +20,10 @@ public class DriveUntillRed_Test extends LinearOpMode {
 
     private static final double DRIVE_POWER = 0.3;
 
-<<<<<<< HEAD
-    private static final float RED_MIN_VALUE = 0.05f;
-    private static final float RED_DOMINANCE_RATIO = 1.5f;
-=======
     private static final float SATURATION_GRAY_THRESHOLD = 0.35f;
     private static final float VALUE_BLACK_THRESHOLD = 0.15f;
     private static final float HUE_RED_MAX = 15f;
     private static final float HUE_RED_MIN = 345f;
->>>>>>> ea805e9 (new auto code in testing)
 
     @Override
     public void runOpMode() {
@@ -90,17 +82,6 @@ public class DriveUntillRed_Test extends LinearOpMode {
 
     private boolean isRed() {
         NormalizedRGBA colors = colorSensor.getNormalizedColors();
-<<<<<<< HEAD
-
-        telemetry.addData("Raw RGB", "%.3f, %.3f, %.3f", colors.red, colors.green, colors.blue);
-
-        if (colors.red < RED_MIN_VALUE) return false;
-
-        return colors.red > colors.green * RED_DOMINANCE_RATIO
-                && colors.red > colors.blue * RED_DOMINANCE_RATIO;
-    }
-}
-=======
         float[] hsv = new float[3];
         Color.colorToHSV(colors.toColor(), hsv);
 
@@ -110,10 +91,10 @@ public class DriveUntillRed_Test extends LinearOpMode {
 
         telemetry.addData("Hue / Sat / Val", "%.1f / %.3f / %.3f", hue, saturation, value);
 
-        if (value < VALUE_BLACK_THRESHOLD) return false;
-        if (saturation < SATURATION_GRAY_THRESHOLD) return false;
+        if (colors.red < RED_MIN_VALUE) return false;
 
-        return hue < HUE_RED_MAX || hue >= HUE_RED_MIN;
+        return colors.red > colors.green * RED_DOMINANCE_RATIO
+                && colors.red > colors.blue * RED_DOMINANCE_RATIO;
     }
 }
 >>>>>>> ea805e9 (new auto code in testing)
